9232ac9e71074909247269755722a1c9db361ae9
AblazeVase69188 <73418189+AblazeVase69188@users.noreply.github.com>
2025-06-17 13:37:55 +0800

    增加配置文件逻辑，增加备份逻辑

--- difftime.py
+++ difftime.py
@@ -1,3 +1,4 @@
+import os
 import time
 import openpyxl
 from datetime import datetime
@@ -6,26 +7,36 @@ from openpyxl.styles import NamedStyle
 
 import base
 
-num_diff = 1000 # 未来可能会改成从配置文件加载
-
 api_url = base.WIKI_API_URL + "?action=compare&format=json&prop=timestamp&formatversion=2"
-minrev = 1
-maxrev = base.get_last_diff()['query']['recentchanges'][0]['revid']
+num_diff, minrev, maxrev = base.difftime_get_config()
+if minrev is None:
+    minrev = 1
+if maxrev is None:
+    maxrev = base.get_last_diff()['query']['recentchanges'][0]['revid']
 
 rev = minrev
+
+if os.path.exists("difftime_temp_backup.xlsx"): # 恢复上次中断时保存的内容
+    wb = openpyxl.load_workbook("difftime_temp_backup.xlsx")
+    ws = wb.active
+    date_style = NamedStyle(name="date_style", number_format="YYYY-MM-DD")
+    wb.add_named_style(date_style)
+    last_rev = ws.cell(row=ws.max_row, column=2).value
+    rev = last_rev + num_diff
+else:
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    date_style = NamedStyle(name="date_style", number_format="YYYY-MM-DD")
+    wb.add_named_style(date_style)
+    ws.column_dimensions['A'].width = 10.89
+    ws.column_dimensions['C'].width = 10.89
+    ws['A1'] = "日期"
+    ws['B1'] = "revid"
+
 fromrev = rev
 torev = rev
 
-wb = openpyxl.Workbook()
-ws = wb.active
-date_style = NamedStyle(name="date_style", number_format="YYYY-MM-DD")
-wb.add_named_style(date_style)
-ws.column_dimensions['A'].width = 10.89
-ws.column_dimensions['C'].width = 10.89
-ws['A1'] = "日期"
-ws['B1'] = "revid"
-
-while rev < maxrev:
+while rev < maxrev: # 主循环
     time.sleep(1)
 
     current_url = api_url + f"&fromrev={fromrev}&torev={torev}"
@@ -53,6 +64,10 @@ while rev < maxrev:
         fromrev = rev
         torev = rev
 
+        if row % 100 == 0:
+            wb.save("difftime_temp_backup.xlsx")
+            print(f"已保存进度至revid：{fromrev}")
+
     elif rev - fromrev > 5 or torev - rev > 5: # 防止大量修订数据被删除时拖慢程序速度
         print(f"{fromrev}-{torev}的差异数据均无法查询！")
 
@@ -60,7 +75,7 @@ while rev < maxrev:
         fromrev = rev
         torev = rev
 
-    else:
+    else: # 修订数据被删除，扩大搜索范围
         fromrev_url = api_url + f"&fromrev={fromrev}&torev={fromrev}"
         torev_url = api_url + f"&torev={torev}&fromrev={torev}"
 
