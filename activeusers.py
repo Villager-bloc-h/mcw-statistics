999b6aab802aa31a7c77918549ca6a48852712ab
AblazeVase69188 <73418189+AblazeVase69188@users.noreply.github.com>
2025-07-14 00:02:33 +0800

    重写时间戳处理逻辑，逻辑更加清晰

--- activeusers.py
+++ activeusers.py
@@ -1,70 +1,61 @@
 import sys
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import openpyxl
 
 import base
 
-# 读取配置，包括：用户组排列顺序和用户组名称
+# 读取配置，包括：用户组排列顺序、用户组名称和模式
 usergroup_order, usergroup_mapping, mode = base.activeusers_get_config()
 
-if mode == "standard" or mode == "debug":
-    pass
+now = datetime.now()
+year = now.year
+month = now.month
+
+if mode == "standard": # 标准模式
+    day1 = 28
+    day2 = 28
+    if month == 2:
+        day2 = 25
+    elif month == 3:
+        day1 = 25
+
+    end_time = now.replace(day=day2, hour=0, minute=0, second=0)
+    start_time = end_time.replace(day=day1, month=month - 1)
+    if start_time.month == 0:
+        start_time = start_time.replace(year=year - 1, month=12)
+
+    start_date_str = f"{start_time.year}年{start_time.month}月{start_time.day}日0时"
+    end_date_str = f"{end_time.year}年{end_time.month}月{end_time.day}日0时"
+
+    excel_filename = f"activeusers-{year}年{month}月.xlsx"
+    txt_filename = f"activeusers-{year}年{month}月.txt"
+
+elif mode == "debug": # 调试模式
+    end_time = now.replace(second=0)
+    start_time = end_time - timedelta(days=30)
+
+    start_date_str = f"{start_time.year}年{start_time.month}月{start_time.day}日{start_time.hour}时{start_time.minute}分"
+    end_date_str = f"{end_time.year}年{end_time.month}月{end_time.day}日{end_time.hour}时{end_time.minute}分"
+
+    excel_filename = end_time.strftime("activeusers-%Y年%m月%d日%H时%M分-调试.xlsx")
+    txt_filename = end_time.strftime("activeusers-%Y年%m月%d日%H时%M分-调试.txt")
+
 else:
     print("指定的模式不存在，请检查配置文件。")
     input("按任意键退出")
     sys.exit(1)
 
-'''
-设置时间段：
-如果当前是2月，那么统计1月28日到2月25日的数据；
-如果当前是3月，那么统计2月25日到3月28日的数据；
-如果不符合上述情况，那么统计上月28日到本月28日的数据。
-
-API只接受UTC时间的时间戳，因此需要根据时区做合适处理。
-'''
-now = datetime.now()
-year = now.year
-month = now.month
-day1 = 28
-day2 = 25
-
-if base.timezone > 0: # 这些时区0点时，UTC时间在上一天
-    day1 -= 1
-    day2 -= 1
-
-if month == 2:
-    start_date = datetime(year, 1, day1)
-    end_date = datetime(year, 2, day2)
-elif month == 3:
-    start_date = datetime(year, 2, day2)
-    end_date = datetime(year, 3, day1)
-else:
-    if month == 1:
-        start_date = datetime(year - 1, 12, day1)
-    else:
-        start_date = datetime(year, month - 1, day1)
-
-    end_date = datetime(year, month, day1)
-
-hour = (24 - base.timezone) % 24
-start_timestamp = start_date.strftime("%Y-%m-%d") + f"T{hour:02d}:00:00Z"
-end_timestamp = end_date.strftime("%Y-%m-%d") + f"T{hour:02d}:00:00Z"
-
-start_utc = datetime.strptime(start_timestamp, "%Y-%m-%dT%H:%M:%SZ")
-end_utc = datetime.strptime(end_timestamp, "%Y-%m-%dT%H:%M:%SZ")
-
-start_local = start_utc + timedelta(hours=base.timezone)
-end_local = end_utc + timedelta(hours=base.timezone)
-
-start_date_str = f"{start_local.year}年{start_local.month}月{start_local.day}日0时"
-end_date_str = f"{end_local.year}年{end_local.month}月{end_local.day}日0时"
-
-excel_filename = f"activeusers-{year}年{month}月.xlsx"
-txt_filename = f"activeusers-{year}年{month}月.txt"
+# API只接受UTC时间的时间戳，因此需要根据时区做合适处理
+end_time = end_time.replace(tzinfo=timezone(timedelta(hours=base.timezone)))
+start_time = start_time.replace(tzinfo=timezone(timedelta(hours=base.timezone)))
+end_time_utc = end_time.astimezone(timezone.utc)
+start_time_utc = start_time.astimezone(timezone.utc)
+end_timestamp_utc = end_time_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+start_timestamp_utc = start_time_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 api_url = base.WIKI_API_URL + "?action=query&format=json&list=recentchanges&formatversion=2&rcprop=user|loginfo&rclimit=500&rctype=edit|new|log"
-api_url = api_url + f"&rcstart={end_timestamp}&rcend={start_timestamp}"
+api_url = api_url + f"&rcstart={end_timestamp_utc}&rcend={start_timestamp_utc}"
 
 # 设置excel表格格式
 wb = openpyxl.Workbook()
@@ -173,10 +164,19 @@ print(f"Excel结果已保存至{excel_filename}")
 
 # 将排序后的内容变为wikitable
 wiki_content = '''{| class="wikitable sortable collapsible"
-|+ %s-%s活跃用户列表
+|+ %s-%s''' % (start_date_str, end_date_str)
+
+if base.timezone > 0:
+    wiki_content = f"{wiki_content} (UTC+{base.timezone}) "
+elif base.timezone < 0:
+    wiki_content = f"{wiki_content} (UTC-{base.timezone}) "
+else:
+    wiki_content = f"{wiki_content} (UTC) "
+
+wiki_content = wiki_content + '''活跃用户列表
 |-
 ! 排名 !! 用户名 !! 操作数 !! 本地用户组
-''' % (start_date_str, end_date_str)
+'''
 
 for idx, (user, count, group_name) in enumerate(sorted_data):
     group_display = group_name
