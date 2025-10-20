adcef9a35a2a558f9f221534e72e41683b117faa
AblazeVase69188 <73418189+AblazeVase69188@users.noreply.github.com>
2025-10-20 15:16:00 +0800

    更改了控制登录的方式

--- base.py
+++ base.py
@@ -6,64 +6,6 @@ import re
 import requests
 import datetime
 
-with open("config.json", "r", encoding="utf-8") as config_file:
-    config = json.load(config_file)
-    wiki = config["wiki"]
-    user_agent = config["user_agent"]
-    timezone = int(config["timezone"])
-    username = config["username"] if "username" in config else None
-    password = config["password"] if "password" in config else None
-
-wiki_lang = ['de', 'en', 'es', 'fr', 'it', 'ja', 'ko', 'lzh', 'nl', 'pt', 'ru', 'th', 'uk', 'zh', 'meta']
-
-if wiki not in wiki_lang:
-    print("不存在此语言的Minecraft Wiki！")
-    input("按回车键退出")
-    sys.exit(1)
-
-elif wiki == 'en':
-    WIKI_BASE_URL = "https://minecraft.wiki"
-
-else:
-    WIKI_BASE_URL = f"https://{wiki}.minecraft.wiki"
-
-WIKI_API_URL = WIKI_BASE_URL + "/api.php"
-
-if username and password:
-    session = requests.Session()
-    session.headers.update({"User-Agent": user_agent})
-
-    # 获取登录token
-    r1 = session.get(WIKI_API_URL, params={
-        'action': 'query',
-        'meta': 'tokens',
-        'type': 'login',
-        'format': 'json'
-    })
-    login_token = r1.json()['query']['tokens']['logintoken']
-
-    # 提交用户名和密码
-    r2 = session.post(WIKI_API_URL, data={
-        'action': 'login',
-        'lgname': username,
-        'lgpassword': password,
-        'lgtoken': login_token,
-        'format': 'json'
-    })
-
-    # 检查登录结果
-    result = r2.json()
-    if result['login']['result'] == 'Success':
-        islogin = True
-        print("登录成功")
-    else:
-        islogin = False
-        print("登录失败：", result['login'])
-
-else:
-    print("未提供用户名和密码，将以未登录状态运作")
-    islogin = False
-
 
 def difftime_get_config():  # difftime.py读取配置
     difftime_config = config.get("difftime", {})
@@ -117,12 +59,52 @@ def checkhiddenrc_get_config():  # checkhiddenrc.py读取配置
     return starttime, endtime
 
 
+def login():  # 尝试登录
+    global session
+
+    if username and password:
+        session = requests.Session()
+        session.headers.update({"User-Agent": user_agent})
+
+        # 获取登录token
+        r1 = session.get(WIKI_API_URL, params={
+            'action': 'query',
+            'meta': 'tokens',
+            'type': 'login',
+            'format': 'json'
+        })
+        login_token = r1.json()['query']['tokens']['logintoken']
+
+        # 提交用户名和密码
+        r2 = session.post(WIKI_API_URL, data={
+            'action': 'login',
+            'lgname': username,
+            'lgpassword': password,
+            'lgtoken': login_token,
+            'format': 'json'
+        })
+
+        # 检查登录结果
+        result = r2.json()
+        if result['login']['result'] == 'Success':
+            print("登录成功")
+        else:
+            print("登录失败：", result['login'])
+            session = None
+
+    else:
+        print("未提供用户名和密码，将以未登录状态运作")
+        session = None
+    
+    return session
+
+
 def get_data(params):  # 从Mediawiki API获取数据
     tries = 0
     while 1:
         try:
-            if islogin:
-                response = session.post(WIKI_API_URL, data=params)
+            if session:
+                response = session.get(WIKI_API_URL, params=params)
             else:
                 response = requests.get(WIKI_API_URL, params=params, headers={"User-Agent": user_agent})
 
@@ -176,3 +158,29 @@ def is_ip_address(s):
             re.match(ipv4_pattern, s) is not None or
             re.match(ipv6_pattern, s) is not None
     )
+
+
+with open("config.json", "r", encoding="utf-8") as config_file:
+    config = json.load(config_file)
+    wiki = config["wiki"]
+    user_agent = config["user_agent"]
+    timezone = int(config["timezone"])
+    username = config["username"] if "username" in config else None
+    password = config["password"] if "password" in config else None
+
+wiki_lang = ['de', 'en', 'es', 'fr', 'it', 'ja', 'ko', 'lzh', 'nl', 'pt', 'ru', 'th', 'uk', 'zh', 'meta']
+
+if wiki not in wiki_lang:
+    print("不存在此语言的Minecraft Wiki！")
+    input("按回车键退出")
+    sys.exit(1)
+
+elif wiki == 'en':
+    WIKI_BASE_URL = "https://minecraft.wiki"
+
+else:
+    WIKI_BASE_URL = f"https://{wiki}.minecraft.wiki"
+
+WIKI_API_URL = WIKI_BASE_URL + "/api.php"
+
+# session = login()
